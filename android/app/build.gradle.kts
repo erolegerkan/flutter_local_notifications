e3eb68fcd3e789408c1d022e578b054785d461f9
erolegerkan <erolege.education@hotmail.com>
2026-02-01 13:17:54 +0300

    chore : setup android side for flutter_local_notifications package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,27 +7,32 @@ plugins {
 
 android {
     namespace = "com.example.flutter_local_notification"
-    compileSdk = flutter.compileSdkVersion
+    // For flutter_local_notification it should be set to 35 at a minimum
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+
+        // For flutter_local_notification setup
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_local_notification"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // For flutter_local_notification setup
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -41,4 +46,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
